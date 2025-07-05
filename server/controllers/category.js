2c198048df68148bb09c4bd3be720e4edccc294d
Phongs4thon <plstr142@gmail.com>
2025-07-06 01:45:25 +0700

    create Category

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -1,6 +1,15 @@
 exports.create = async (req, res) => {
   try {
-    res.send("Hello Category");
+    res.send("Hello create Category");
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
+exports.list = async (req, res) => {
+  try {
+    res.send("Hello list Category");
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Server Error" });
--- server/routes/category.js
+++ server/routes/category.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { create } = require("../controllers/category");
+const { create } = require("../controllers/category.js");
 
 router.post("/category", create);
 router.get("/category");
