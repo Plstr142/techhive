9a7dc9f3648775271c971084ea68f49974b5bf44
Phongs4thon <plstr142@gmail.com>
2025-07-07 11:27:21 +0700

    Create a config folder inside prisma.js and create an auth function to register and log in using bcrypt and a JWT token.

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,3 +1,7 @@
+const prisma = require("../config/prisma.js");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
 exports.register = async (req, res) => {
   try {
     // destructuring
@@ -12,9 +16,30 @@ exports.register = async (req, res) => {
     }
 
     // Step 2 Check Email in DB already ?
+    const user = await prisma.user.findFirst({
+      // check field in table db
+      where: {
+        email: email,
+      },
+    });
 
-    console.log(email, password);
-    res.send("Hello Register In Controller");
+    if (user) {
+      return res.status(400).json({ message: "Email already exists!" });
+    }
+
+    // Step 3 Hash Password
+    const hashPassword = await bcrypt.hash(password, 10);
+    console.log(hashPassword);
+
+    // Step 4 Register
+    await prisma.user.create({
+      data: {
+        email: email,
+        password: hashPassword,
+      },
+    });
+
+    res.send("Register Success");
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Server Error" });
@@ -23,8 +48,45 @@ exports.register = async (req, res) => {
 
 exports.login = async (req, res) => {
   try {
-    //code
-    res.send("Hello Login In Controller");
+    // destructuring
+    const { email, password } = req.body;
+
+    // Step 1 Check Email
+    const user = await prisma.user.findFirst({
+      where: {
+        email: email,
+      },
+    });
+    if (!user || !user.enabled) {
+      return res.status(400).json({ message: "User not found or not enabled" });
+    }
+    // Step 2 Check Password
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ message: "Password Invalid!" });
+    }
+    // Step 3 Create Payload (body)
+    const payload = {
+      id: user.id,
+      email: user.email,
+      role: user.role,
+    };
+    // Step 4 Generate Token
+    jwt.sign(
+      payload,
+      process.env.SECRET,
+      // expires in ? day/hour/minutes
+      { expiresIn: "7d" },
+      (error, token) => {
+        if (error) {
+          return res.status(500).json({ message: "Server Error" });
+        }
+
+        // send to client
+        res.json({ payload, token });
+      }
+    );
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Server Error" });
